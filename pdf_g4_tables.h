eafec9bc311926917260dfc6e7d051c8fb7aca55
Eric Smith <spacewar@gmail.com>
2003-02-21 12:28:37 +0000

    GPL

--- pdf_g4_tables.h
+++ pdf_g4_tables.h
@@ -1,3 +1,29 @@
+/*
+ * t2p: Create a PDF file from the contents of one or more TIFF
+ *      bilevel image files.  The images in the resulting PDF file
+ *      will be compressed using ITU-T T.6 (G4) fax encoding.
+ *
+ * G4 encoding tables
+ * $Id: pdf_g4_tables.h,v 1.2 2003/02/21 04:28:37 eric Exp $
+ * Copyright 2001, 2002, 2003 Eric Smith <eric@brouhaha.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.  Note that permission is
+ * not granted to redistribute this program under the terms of any
+ * other version of the General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+ */
+
+
 typedef struct
 {
   uint32_t count;
