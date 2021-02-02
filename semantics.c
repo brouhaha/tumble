34e4eed8a9fdbfbd6bdb558042a5dc27f50f82eb
Jonathan Gray <jsg@jsg.id.au>
2021-02-02 16:29:22 +1100

    fix -fno-common build
    clang 11 and gcc 10 default to -fno-common.  Mark some variables extern
    to fix the build.

--- semantics.c
+++ semantics.c
@@ -119,7 +119,7 @@ typedef struct output_page_t
 #endif
 
 
-FILE *yyin;
+extern FILE *yyin;
 int line;  /* line number in spec file */
 
 int bookmark_level;
--- tumble_input.h
+++ tumble_input.h
@@ -71,4 +71,4 @@ void init_jpeg_handler (void);
 void init_pbm_handler  (void);
 void init_png_handler  (void);
 
-input_handler_t blank_handler;
+extern input_handler_t blank_handler;
